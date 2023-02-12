ae1e540f391b59cb117603ce3947549bbb338eba
Dennis <mwendadennis8779@gmail.com>
2023-02-12 20:00:47 +0300

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
+    self[-1] == "?"
 
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    split(/[.?!]+/).count
   end
 end
+
+"".end_with?(".")
